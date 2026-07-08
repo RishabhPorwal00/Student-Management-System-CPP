62e4458a5e15c5ae7fffda24256844c2fb14ad65
RishabhPorwal00 <rishabhporwal06@gmail.com>
2026-07-08 20:01:51 +0530

    Add addStudent and viewStudents functions
    Added methods to add and view students in the StudentManager class.

--- StudentManager.cpp
+++ StudentManager.cpp
@@ -33,3 +33,45 @@ void StudentManager::saveToFile()
 
     file.close();
 }
+void StudentManager::addStudent()
+{
+    Student s;
+
+    cout << "\nEnter Student ID: ";
+    cin >> s.id;
+
+    cout << "Enter Name: ";
+    cin >> s.name;
+
+    cout << "Enter Age: ";
+    cin >> s.age;
+
+    cout << "Enter Course: ";
+    cin >> s.course;
+
+    students.push_back(s);
+
+    saveToFile();
+
+    cout << "\nStudent Added Successfully!\n";
+}
+
+void StudentManager::viewStudents()
+{
+    if (students.empty())
+    {
+        cout << "\nNo Students Found!\n";
+        return;
+    }
+
+    cout << "\n------ Student List ------\n";
+
+    for (Student s : students)
+    {
+        cout << "ID: " << s.id << endl;
+        cout << "Name: " << s.name << endl;
+        cout << "Age: " << s.age << endl;
+        cout << "Course: " << s.course << endl;
+        cout << "--------------------------\n";
+    }
+}

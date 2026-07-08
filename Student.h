9f8bc6626ccbb698449e64b981e12a31e7ffa88f
RishabhPorwal00 <rishabhporwal06@gmail.com>
2026-07-08 19:52:47 +0530

    Create Student.h

---
+++ Student.h
@@ -0,0 +1,26 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    int id;
+    string name;
+    int age;
+    string course;
+
+    Student() {}
+
+    Student(int i, string n, int a, string c)
+    {
+        id = i;
+        name = n;
+        age = a;
+        course = c;
+    }
+};
+
+#endif

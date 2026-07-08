53d6f0a92c88d0dea06dc148d64da789a98fca3e
RishabhPorwal00 <rishabhporwal06@gmail.com>
2026-07-08 20:00:02 +0530

    Add file handling for student data management
    Implement methods to load and save student data from/to a file.

---
+++ StudentManager.cpp
@@ -0,0 +1,36 @@
+#include "StudentManager.h"
+#include <fstream>
+
+void StudentManager::loadFromFile()
+{
+    students.clear();
+
+    ifstream file("students.txt");
+
+    if (!file)
+        return;
+
+    Student s;
+
+    while (file >> s.id >> s.name >> s.age >> s.course)
+    {
+        students.push_back(s);
+    }
+
+    file.close();
+}
+
+void StudentManager::saveToFile()
+{
+    ofstream file("students.txt");
+
+    for (Student s : students)
+    {
+        file << s.id << " "
+             << s.name << " "
+             << s.age << " "
+             << s.course << endl;
+    }
+
+    file.close();
+}
